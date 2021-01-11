15600cf5ac748bdeb22a14c99626bc1554350c7f
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-11 16:25:56 -0600

    add update todo function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,7 @@ import Todo from './Todo'
 
 function TodoList() {
     const [todos, setTodos] = useState([])
+
     const addTodo = todo => {
         if(!todo.text || /^\s*$/.test(todo.text)){
             return;
@@ -11,6 +12,13 @@ function TodoList() {
         const newTodos = [todo, ...todos]
         setTodos(newTodos)
     }
+
+     const updateTodo = (todoId, newValue) => {
+         if(!newValue.text|| /^\s*$/.test(newValue.text)){
+            return;
+        }
+        setTodos(prev => prev.map(item =>(item.id === todoId ? newValue : item)))
+     }
 
     const removeTodo = id => {
         const removeArr = [...todos].filter(todo => todo.id !== id)
@@ -35,6 +43,7 @@ function TodoList() {
             todos={todos}
             completeTodo={completeTodo}
             removeTodo={removeTodo}
+            updateTodo={updateTodo}
             /> 
         </div>
     )
