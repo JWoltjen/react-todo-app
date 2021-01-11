a925ce972dae1921d03677981af49ed530e2ade1
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-11 14:58:50 -0600

    CreateTodoList component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,15 +2,29 @@ import React, { useState } from 'react'
 
 function TodoForm() {
     const [input, setInput] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault(); 
+        // props.onSubmit({
+        //     id: Date.now(),
+        //     text: input
+        // })
+        setInput('')
+    }
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
     return (
         <div>
-            <form className='todo-form'>
+            <form className='todo-form' onSubmit={handleSubmit}>
                 <input 
                 type='text' 
                 placeholder='add a todo' 
                 value={input} 
                 name="text"
                 className='todo-input'
+                onChange={handleChange}
                 />
                 <button className='todo-button'>Add todo</button>
             </form>
