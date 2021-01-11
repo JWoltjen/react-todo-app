c78819fda34f55c67f864f6337252366f8920be5
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-11 16:20:12 -0600

    build remove todo function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo'
 
 function TodoList() {
     const [todos, setTodos] = useState([])
@@ -9,12 +10,32 @@ function TodoList() {
         }
         const newTodos = [todo, ...todos]
         setTodos(newTodos)
-        console.log(...todos)
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
     }
     return (
         <div>
             <h1> What are we doing today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo 
+            todos={todos}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+            /> 
         </div>
     )
 }
